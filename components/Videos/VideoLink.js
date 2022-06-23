acc4f52ad66c8b0bc4d9fea9ccea7028b3ab8595
Sudipto Ghosh <sudipto@ghosh.pro>
2022-06-23 21:12:51 +0530

    minor: video links breaking on next lines

--- components/Videos/VideoLink.js
+++ components/Videos/VideoLink.js
@@ -3,7 +3,7 @@ export default function VideoLink(video) {
     <h2 className="text-md mb-3">
       <a href={video.link} target="_blank" rel="noopener noreferrer">
         <i className="bi bi-arrow-right mr-2"></i>
-        <span className="font-bold capitalize hover:underline" type="button"> {video.title}</span>
+        <span className="font-bold capitalize hover:underline"> {video.title}</span>
       </a>
     </h2>
   );
