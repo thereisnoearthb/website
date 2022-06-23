ed4db07250151e32ff072a6e384996c88e94267d
Sudipto Ghosh <sudipto@ghosh.pro>
2022-06-23 19:12:40 +0530

    add github link

--- components/Home/Socials.js
+++ components/Home/Socials.js
@@ -2,16 +2,19 @@ export default function Socials() {
   return (
     <>
       <a href='https://www.instagram.com/thereisnoearthb/' target={'_blank'} rel={'noopener noreferrer'} className="cursor-pointer">
-        <i className='bi bi-instagram px-5'></i><span className="hidden">instagram</span>
+        <i className='bi bi-instagram px-3'></i><span className="hidden">instagram</span>
       </a>
       <a href='https://twitter.com/ThereIsNoEarthB' target={'_blank'} rel={'noopener noreferrer'} className="cursor-pointer">
-        <i className='bi bi-twitter px-5'></i><span className="hidden">twitter</span>
+        <i className='bi bi-twitter px-3'></i><span className="hidden">twitter</span>
       </a>
       <a href='https://www.facebook.com/thereisnoearthb/' target={'_blank'} rel={'noopener noreferrer'} className="cursor-pointer">
-        <i className='bi bi-facebook px-5'></i><span className="hidden">facebook</span>
+        <i className='bi bi-facebook px-3'></i><span className="hidden">facebook</span>
       </a>
       <a href='https://www.linkedin.com/in/thereisnoearthb' target={'_blank'} rel={'noopener noreferrer'} className="cursor-pointer">
-        <i className='bi bi-linkedin px-5'></i><span className="hidden">linkedin</span>
+        <i className='bi bi-linkedin px-3'></i><span className="hidden">linkedin</span>
+      </a>
+      <a href='https://github.com/thereisnoearthb/website' target={'_blank'} rel={'noopener noreferrer'} className="cursor-pointer">
+        <i className='bi bi-github px-3'></i><span className="hidden">github</span>
       </a>
     </>
   );
